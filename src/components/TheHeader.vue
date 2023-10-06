e434a5045260d29188cfaa89ac8d1fb9d81a8fa1
Angus Hsu <42315780+Billi0ns@users.noreply.github.com>
2023-10-06 16:34:46 +0800

    feat: add header

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,14 @@
+<template>
+  <header
+    class="fixed top-0 left-0 w-full bg-white py-16px px-24px shadow-lg flex justify-center items-center"
+  >
+    <h1 class="text-xl font-bold">Repo River</h1>
+  </header>
+</template>
+
+<style>
+/* Set the height of the body to account for the fixed header */
+body {
+  padding-top: 64px;
+}
+</style>
