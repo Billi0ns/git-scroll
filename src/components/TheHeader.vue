9a99aff55d75e1feb3745f3dd2dd0e263a38ce0e
Angus Hsu <42315780+Billi0ns@users.noreply.github.com>
2023-10-07 18:09:08 +0800

    feat: add github link

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -2,12 +2,9 @@
 import { useDark, useToggle } from '@vueuse/core';
 import IconLightMode from '@/components/icons/IconLightMode.vue';
 import IconDarkMode from '@/components/icons/IconDarkMode.vue';
+import IconGithub from '@/components/icons/IconGithub.vue';
 
-const isDark = useDark({
-  attribute: 'data-scheme',
-  valueDark: 'dark',
-  valueLight: 'light',
-});
+const isDark = useDark();
 const toggleDark = useToggle(isDark);
 </script>
 <template>
@@ -15,8 +12,14 @@ const toggleDark = useToggle(isDark);
     class="fixed top-0 left-0 w-full bg-$header-bg py-16px px-24px shadow-lg flex justify-center items-center"
   >
     <div class="container flex items-center justify-between">
-      <h1 class="text-xl text-$header-text font-bold">Evan You's Repo</h1>
+      <h1 class="text-xl text-$header-text font-bold">Evan You's Repos</h1>
       <div class="flex justify-center items-center">
+        <a
+          href="https://github.com/Billi0ns/git-scroll"
+          class="cursor-pointer flex justify-center items-center mr-20px sm:mr-30px"
+        >
+          <IconGithub class="text-black dark:text-white" />
+        </a>
         <IconDarkMode v-if="isDark" class="cursor-pointer" @click="toggleDark()" />
         <IconLightMode v-else class="cursor-pointer" @click="toggleDark()" />
       </div>
--- src/components/icons/IconDarkMode.vue
+++ src/components/icons/IconDarkMode.vue
@@ -1,9 +1,8 @@
-<!-- This icon is from <https://github.com/Templarian/MaterialDesign>, distributed under Apache 2.0 (https://www.apache.org/licenses/LICENSE-2.0) license-->
 <template>
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width="24"
-    height="24"
+    width="22"
+    height="22"
     viewBox="0 0 24 24"
     fill="none"
     stroke="white"
